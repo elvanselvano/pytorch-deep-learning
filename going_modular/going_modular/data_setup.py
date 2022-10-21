c42d06862ece1029ed0d53243bcc734ce5543bed
Elvan Selvano <elvanselvano71@gmail.com>
2022-10-21 14:41:15 +0700

    Add valid dataloader

--- going_modular/going_modular/data_setup.py
+++ going_modular/going_modular/data_setup.py
@@ -10,7 +10,8 @@ from torch.utils.data import DataLoader
 NUM_WORKERS = os.cpu_count()
 
 def create_dataloaders(
-    train_dir: str, 
+    train_dir: str,
+    valid_dir: str,
     test_dir: str, 
     transform: transforms.Compose, 
     batch_size: int, 
@@ -18,22 +19,24 @@ def create_dataloaders(
 ):
   """Creates training and testing DataLoaders.
 
-  Takes in a training directory and testing directory path and turns
+  Takes in a trainingdirectory, validation directory, and testing directory path and turns
   them into PyTorch Datasets and then into PyTorch DataLoaders.
 
   Args:
     train_dir: Path to training directory.
+    valid_dir: Path to validation directory.
     test_dir: Path to testing directory.
     transform: torchvision transforms to perform on training and testing data.
     batch_size: Number of samples per batch in each of the DataLoaders.
     num_workers: An integer for number of workers per DataLoader.
 
   Returns:
-    A tuple of (train_dataloader, test_dataloader, class_names).
+    A tuple of (train_dataloader, valid_dataloader, test_dataloader, class_names).
     Where class_names is a list of the target classes.
     Example usage:
-      train_dataloader, test_dataloader, class_names = \
+      train_dataloader, valid_dataloader, test_dataloader, class_names = \
         = create_dataloaders(train_dir=path/to/train_dir,
+                             valid_dir=path/to/valid_dir,
                              test_dir=path/to/test_dir,
                              transform=some_transform,
                              batch_size=32,
@@ -41,6 +44,7 @@ def create_dataloaders(
   """
   # Use ImageFolder to create dataset(s)
   train_data = datasets.ImageFolder(train_dir, transform=transform)
+  valid_data = datasets.ImageFolder(valid_dir, transform=transform)
   test_data = datasets.ImageFolder(test_dir, transform=transform)
 
   # Get class names
@@ -54,6 +58,13 @@ def create_dataloaders(
       num_workers=num_workers,
       pin_memory=True,
   )
+  valid_dataloader = DataLoader(
+      valid_data,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
   test_dataloader = DataLoader(
       test_data,
       batch_size=batch_size,
@@ -62,4 +73,4 @@ def create_dataloaders(
       pin_memory=True,
   )
 
-  return train_dataloader, test_dataloader, class_names
+  return train_dataloader, valid_dataloader, test_dataloader, class_names
